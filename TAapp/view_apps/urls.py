47663bb61ce259721a741d1617dbd7e8e93f76f9
Samuel D Forbush <54643575+SamForbush@users.noreply.github.com>
2023-03-20 14:41:57 -0400

    Added create_course.html for course creation form

--- TAapp/view_apps/urls.py
+++ TAapp/view_apps/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'view_apps'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create_course', views.create_course, name='create_course'),
 
 ]
--- TAapp/view_apps/views.py
+++ TAapp/view_apps/views.py
@@ -10,3 +10,8 @@ def index(request):
     context = {'latest_course_list': latest_course_list, 'users': users}
     return render(request, 'view_apps/index.html', context)
 
+def create_course(request):
+    users = User.objects.all()
+    latest_course_list = Course.objects.all()
+    context = {'latest_course_list': latest_course_list, 'users': users}
+    return render(request, 'view_apps/create_course.html', context)
