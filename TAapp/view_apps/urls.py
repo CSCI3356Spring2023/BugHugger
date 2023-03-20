0f03692c9dfe309e8f915898cef63bab43f1b52b
Samuel D Forbush <54643575+SamForbush@users.noreply.github.com>
2023-03-20 14:20:39 -0400

    Wireframe of student view of apps

--- TAapp/view_apps/urls.py
+++ TAapp/view_apps/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'view_apps'
 urlpatterns = [
     path('', views.index, name='index'),
+
 ]
--- TAapp/view_apps/views.py
+++ TAapp/view_apps/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
 from .models import Course
+from login.models import User
 
 
 def index(request):
-    latest_course_list = Course.objects.order_by('-pub_date')[:5]
-    context = {'latest_course_list': latest_course_list}
+    users = User.objects.all()
+    latest_course_list = Course.objects.all()
+    context = {'latest_course_list': latest_course_list, 'users': users}
     return render(request, 'view_apps/index.html', context)
 
