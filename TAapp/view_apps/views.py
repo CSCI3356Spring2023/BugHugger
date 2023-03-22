49cc4d371e46631128b8d8c5369a18447a1da69f
GcodeS <grantsheppard@me.com>
2023-03-22 00:50:49 -0400

    Revert "Merge branch 'main' of https://github.com/CSCI3356Spring2023/BugHugger"
    This reverts commit bfbccdc4a35705e1437fb8a9f70fa759f07fe01f, reversing
    changes made to fea65180f78af4218d01cf9931f81f9cae03e9a9.

--- TAapp/view_apps/views.py
+++ TAapp/view_apps/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 
 from .models import Course, App
 from datetime import datetime
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 
 def is_student(user):
     return user.groups.filter(name='Student').exists()
@@ -27,13 +27,7 @@ def create_course(request):
         professor_name = request.POST['name']
         title = request.POST['course_name']
         description = request.POST['course_description']
-        id = request.POST['course_id']
-        time = request.POST['time_text']
-        section = request.POST['section']
-        num_sections = request.POST['num_sections']
-        oh = request.POST['office_hours']
-        ta = request.POST['TAs']
-        c = Course(course_title = title, description_text = description, professor_text = professor_name, pub_date = datetime.now(), course_id = id, has_meetings = True, has_discussion = True, section = section, num_sections = num_sections, num_office_hours = oh, num_tas = ta, time_text = time)
+        c = Course(course_title = title, description_text = description, professor_text = professor_name, pub_date = datetime.now(), course_id = "NEW", has_meetings = True, has_discussion = True, section = 1, num_sections = 4, num_office_hours = 6, num_tas = 4)
         c.save()
         return redirect('/view_apps/')
     else:
