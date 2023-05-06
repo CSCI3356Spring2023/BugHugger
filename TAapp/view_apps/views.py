4f0cd28547c529da6a414a6e69e91f12255dadfe
Peiyu Zhong <113380501+xxKiddMdx@users.noreply.github.com>
2023-05-05 21:56:19 -0400

    Update views.py

--- TAapp/view_apps/views.py
+++ TAapp/view_apps/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout #Added by Aidan
 from .models import Course, App
 from datetime import datetime
-from django.utils import timezone
 from django.core.mail import send_mail
 import random
 from login.models import Prof_profile, Stud_profile
 from django.conf import settings
+
+
 
 #variables for course names
 COURSE_NAMES = ["Computer Science I", "Computer Science II", "Object Oriented Design",
@@ -176,8 +177,10 @@ def deny(request, id):
 
 def create_course(request):
     if request.method == "POST":
-        email = request.user.username
-        professor_name = request.POST['name']
+        user = request.user
+        professor_profile = Prof_profile.objects.get(user=user)
+        professor_email = professor_profile.email
+        professor_name = f"{professor_profile.first_name} {professor_profile.last_name}"
         title = request.POST['course_name']
         description = request.POST['course_description']
         id = request.POST['course_id']
@@ -195,8 +198,8 @@ def create_course(request):
         if disc == 'yes':
             disc_val = True
 
-        c = Course(assigned_to_email = email, time_text=time, course_title=title, description_text=description,
-                   professor_text=professor_name, pub_date=timezone.now(), course_id=id, has_meetings=meet_val,
+        c = Course(assigned_to_email = request.user.username, time_text=time, course_title=title, description_text=description,
+                   professor_text=professor_name, email_text=professor_email, pub_date=datetime.now(), course_id=id, has_meetings=meet_val,
                    has_discussion=disc_val, section=section, num_sections=num_sections, num_office_hours=oh, num_tas=ta)
         c.save()
         send_mail(
@@ -204,7 +207,7 @@ def create_course(request):
             title,
             settings.EMAIL_HOST_USER,
             ['zhongpd@bc.edu'],
-            fail_silently=True,
+            fail_silently=False,
         )
         return redirect('/view_apps/')
     else:
@@ -215,6 +218,9 @@ def apply(request, id):
     MAX_APPLICATIONS = 5
     if request.method == "POST":
         user=request.user
+        student_profile = Stud_profile.objects.get(user=user)
+        student_email = student_profile.email
+        student_name = f"{student_profile.first_name} {student_profile.last_name}"
         if App.objects.filter(user=user).exists():
             c = App.objects.filter(user=user).count()
             num_uses = c + 1
@@ -225,7 +231,7 @@ def apply(request, id):
             else:
                 #IMPORTANT: Assuming course_id is unique
                 a = random.randint(10000000, 99999999)
-                c = App(user=user, num_uses = num_uses, office_hours=office_hours, why_ta=why_ta, id = str(a))
+                c = App(user=user, student_text=student_name, email_text=student_email, num_uses = num_uses, office_hours=office_hours, why_ta=why_ta, id = str(a))
                 current_apps = Course.objects.get(course_id=course_id).applications
                 current_apps += " " + c.id
                 Course.objects.filter(course_id = course_id).update(applications = current_apps)
@@ -237,7 +243,7 @@ def apply(request, id):
             office_hours = request.POST['office_hours']
             why_ta = request.POST['why_ta']
             a = random.randint(10000000, 99999999)
-            c = App(user=user, office_hours=office_hours, why_ta=why_ta, num_uses=num_uses, id = str(a))
+            c = App(user=user, student_text=student_name, email_text=student_email, office_hours=office_hours, why_ta=why_ta, num_uses=num_uses, id = str(a))
             # IMPORTANT: Assuming course_id is unique
             current_apps = Course.objects.get(course_id=course_id).applications
             current_apps += " " + c.id
